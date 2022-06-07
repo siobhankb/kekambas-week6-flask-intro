ca71fa10eca3716f385513f59f3ea6f40d83ec31
siobhankb <siobhan.boylan@gmail.com>
2022-06-07 11:31:47 -0500

    set base template and code blocks

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 # this points to module where app is running
 app = Flask(__name__)
 #tell what URL
-@app.route('/')
+@app.route('/index')
 def index():
     user = {
         'username': 'siobhankb',
@@ -13,9 +13,9 @@ def index():
     colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
     return render_template('index.html', user=user, colors=colors)
 
-@app.route("/test")
-def test():
-    return 'This is a test'
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 
 # when you close the terminal, it doesn't save the app you've run
 # python has a package that will automatically
