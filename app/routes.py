984916eeeb9660e4a60ab6701b4cdbea1b46d826
siobhankb <siobhan.boylan@gmail.com>
2022-06-07 16:58:20 -0500

    caught up

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash
 from app.forms import SignUpForm
 
 #tell what URL
@@ -14,9 +14,26 @@ def index():
 
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm()
+    if form.validate_on_submit():
+        print('This was a huge success')
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        if username in {'abc', 'aaa'}:
+            flash('That username already exitsts')
+            return redirect(url_for('index'))
+
+        # add the user to the database
+
+        # show message of success/failure
+        flash('You have successfully signed up!', 'success')
+        #redirect back to the homepage
+        return redirect(url_for('index'))
+
     return render_template('signup.html', form=form)
 
 # when you close the terminal, it doesn't save the app you've run
